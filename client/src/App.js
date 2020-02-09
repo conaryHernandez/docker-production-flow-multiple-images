0449a577016c5bca1e43a94eeaef4e598837e24c
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-02-09 15:04:14 -0600

    Adding docker files for multi images workflow
    Commited .env file because it only contains a fix for windows env

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
+
+console.log('app?');
 
 class App extends Component {
   render() {
@@ -14,11 +16,13 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
             <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
+            <Link to="/otherpage">Other Page link</Link>
           </header>
           <div>
-            <Route exact path="/" component={Fib} />
-            <Route path="/otherpage" component={OtherPage} />
+            <Switch>
+              <Route exact path="/" component={Fib} />
+              <Route path="/otherpage" component={OtherPage} />
+            </Switch>
           </div>
         </div>
       </Router>
--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <div>
-      Im some other page
+      Im some other page modified
+      <br />
       <Link to="/">Go back to home page!</Link>
     </div>
   );
